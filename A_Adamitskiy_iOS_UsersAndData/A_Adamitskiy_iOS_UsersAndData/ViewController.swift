a82b7ab334f12725664c797de433ca908fe8c234
Anatoliy Adamitskiy <anato1iy@fullsail.edu>
2015-04-13 10:37:52 -0400

    Initial Commit

---
+++ A_Adamitskiy_iOS_UsersAndData/A_Adamitskiy_iOS_UsersAndData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A_Adamitskiy_iOS_UsersAndData
+//
+//  Created by Anatoliy Adamitskiy on 4/13/15.
+//  Copyright (c) 2015 Anatoliy Adamitskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
